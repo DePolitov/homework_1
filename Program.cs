1ccbed69ff1b90c63a89f328b4c74f16e62079a0
dpolitov <dannn83@mail.ru>
2022-12-03 11:03:11 +0300

    \create exx001

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите первое число=");
+string number1 = Console.ReadLine()!;
+Console.Write("Введите второе число=");
+string number2 = Console.ReadLine()!;
+int c = int.Parse(number1);
+int d = int.Parse(number2);
+if (c > d)
+{
+    Console.WriteLine($"Максимальное число {c}\n");
+}
+else
+{
+    Console.WriteLine($"Максимальное число {d}\n");
+}
